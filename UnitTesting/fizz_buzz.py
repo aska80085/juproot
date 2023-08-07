69f63182eb1f07b44417c204f4b1c6f3d0ea2fc5
oneglyde <oneglyde@gmail.com>
2023-08-07 05:30:21 +0300

    readme

---
+++ UnitTesting/fizz_buzz.py
@@ -0,0 +1,5 @@
+def get_reply(number):
+    if number % 3 == 0 and number % 5 == 0: return 'FizzBuzz'
+    elif number % 3 == 0: return 'Fizz'
+    elif number % 5 == 0: return 'Buzz'
+    else: return ''
